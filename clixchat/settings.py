bb95d9d279e96ead7bc64cccb2468926a044b15c
Brandon <bhanks@mit.edu>
2017-03-28 22:56:05 -0400

    added gitignore, updating configs

--- clixchat/settings.py
+++ clixchat/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bot',
+    'editor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,6 +80,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'postgres': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'clixchat',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',                   # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #     'PORT': '',                   # Set to empty string for default.
+    # }
+
 }
 
 
