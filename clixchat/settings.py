ca21b8f86af58cb3b4bfff98c6c98131f148a44b
Brandon <bhanks@mit.edu>
2017-03-31 01:49:27 -0400

    fixing static

--- clixchat/settings.py
+++ clixchat/settings.py
@@ -103,6 +103,21 @@ if dbconfig:
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT=True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+
+
 
 
 # Password validation
