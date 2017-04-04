93c1addb12547b54984b020dfa58c583ae3bb557
Brandon <bhanks@mit.edu>
2017-04-04 13:16:43 -0400

    added new models to admin interface

--- bot/admin.py
+++ bot/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Command, Response
+from .models import Command, Response, Unit, Session, Activity
 
 admin.site.register(Command)
 admin.site.register(Response)
+admin.site.register(Unit)
+admin.site.register(Session)
+admin.site.register(Activity)
