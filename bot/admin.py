4bedd6ca4b8a6a2a335a66f6d99bb69326f58a61
Brandon <bhanks@mit.edu>
2017-04-04 18:21:38 -0400

    added mptt models, pushing to heroku

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,16 @@
+# from django.contrib import admin
+#
+# from .models import Command, Response, Unit, Session, Activity
+#
+# admin.site.register(Command)
+# admin.site.register(Response)
+# admin.site.register(Unit)
+# admin.site.register(Session)
+# admin.site.register(Activity)
+
+
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Unit
 
-from .models import Command, Response, Unit, Session, Activity
-
-admin.site.register(Command)
-admin.site.register(Response)
-admin.site.register(Unit)
-admin.site.register(Session)
-admin.site.register(Activity)
+admin.site.register(Unit, MPTTModelAdmin)
--- bot/models.py
+++ bot/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
 
 class Command(models.Model):
     command_text = models.SlugField(max_length=100, unique=True)
@@ -14,19 +13,30 @@ class Response(models.Model):
         return self.response_text
 
 
-class Unit(models.Model):
+# class Unit(models.Model):
+#     unit_text = models.SlugField(max_length=100)
+#     def __str__(self):
+#         return self.unit_text
+
+
+class Unit(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
     unit_text = models.SlugField(max_length=100)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
     def __str__(self):
-        return self.unit_text
+        return self.name
 
-class Session(models.Model):
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    session_text = models.SlugField(max_length=100)
-    def __str__(self):
-        return self.session_text
-
-class Activity(models.Model):
-    session = models.ForeignKey(Session, on_delete=models.CASCADE)
-    activity_text = models.SlugField(max_length=100)
-    def __str__(self):
-        return self.activity_text
+    class MPTTMeta:
+        order_insertion_by = ['name']
+#
+# class Session(models.Model):
+#     unit = models.TreeForeignKey(Unit, on_delete=models.CASCADE)
+#     session_text = models.SlugField(max_length=100)
+#     def __str__(self):
+#         return self.session_text
+#
+# class Activity(models.Model):
+#     session = models.TreeForeignKey(Session, on_delete=models.CASCADE)
+#     activity_text = models.SlugField(max_length=100)
+#     def __str__(self):
+#         return self.activity_text
