81e41ef1107579ea5f7e45552b8db2e02c5e20b6
Brandon <bhanks@mit.edu>
2017-04-11 13:55:15 -0400

    tweaking database fields to allow blank values

--- bot/models.py
+++ bot/models.py
@@ -20,9 +20,9 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Element(MPTTModel):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True)
     # default button response = nav back
-    message_text = models.CharField(max_length=200)
+    message_text = models.CharField(max_length=200, blank=True)
 
     # command : the name of the button above it
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
