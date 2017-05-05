7944a339c56b86f76667c35d6947e0570ab9b37e
wx <afoun@wellesley.edu>
2017-05-05 15:39:19 -0400

    printing children and sibs

--- bot/views.py
+++ bot/views.py
@@ -78,22 +78,26 @@ def on_chat_message(msg):
                         reply_markup=ReplyKeyboardMarkup(
                                     keyboard=buttons))
 
+    elif msg['text'] == "Back":
+        last_element = Element.objects.get(pk=user.last_node.pk)
+        print("**last elt, ", last_element)
+        parent = last_element.parent
+        print("**parent, ", parent)
+        sibs = last_element.get_siblings()
+        print("**sibs: ", sibs)   
         
     # not /start or back
     else:
-        #if msg['text'] == "Back":
-         #   last_element = Element.objects.get(pk=user.last_node.pk)
-          #  print("**last elt, ", last_element)
-           # parent = last_element.parent
-            #print("**parent, ", parent)
-            #sibs = last_element.get_siblings()
-            #print("**sibs: ", sibs)
         # print('user last node pk: ', user.last_node.pk)
         last_element = Element.objects.get(pk=user.last_node.pk)       
         msg_pk = user.last_node.pk
         children = last_element.get_children()
-        # print("children: ", children)
-        # print("last element: ", last_element)
+        print("children: ", children)
+        print("last element: ", last_element)
+        parent = last_element.parent
+        print("**parent, ", parent)
+        sibs = last_element.get_siblings()
+        print("**sibs: ", sibs)   
         found = False
         for x in children:
             # print('msg text: ', chat_text)
