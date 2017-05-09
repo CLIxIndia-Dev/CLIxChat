9951ff7d305875049afbbb1099497532c4c5e7dc
wx <afoun@wellesley.edu>
2017-05-09 14:05:52 -0400

    still trying to get the name of the element

--- bot/views.py
+++ bot/views.py
@@ -89,7 +89,7 @@ def on_chat_message(msg):
         print("ELEMENT: ", element)
         filter_element = Element.objects.filter(level=0)
         print("FILTER-ELEMENT: ", filter_element)
-        filter_name = Element.objects.filter(level=0)["Element"]
+        filter_name = Element.objects.filter(level=0)[0].name
         print("NAME: ", filter_name)
         #children = element.get_children()
         #for x in children:
