8d43d39abeec83e0fcf500bf79fff52e33d27147
Brandon <bhanks@mit.edu>
2017-06-19 18:19:54 -0400

    changed to newer listener again

--- bot/views.py
+++ bot/views.py
@@ -275,32 +275,35 @@ def on_chat_message(msg):
     user.save()
 
 bot = telepot.Bot(TOKEN)
-update_queue = Queue()  # channel between `app` and `bot`
+# update_queue = Queue()  # channel between `app` and `bot`
+#
+# bot.message_loop({'chat': on_chat_message #,
+#                   # 'callback_query': on_callback_query,
+#                   # 'inline_query': on_inline_query,
+#                   # 'chosen_inline_result': on_chosen_inline_result
+#                   }, source=update_queue)  # take updates from queue
 
-bot.message_loop({'chat': on_chat_message #,
-                  # 'callback_query': on_callback_query,
-                  # 'inline_query': on_inline_query,
-                  # 'chosen_inline_result': on_chosen_inline_result
-                  }, source=update_queue)  # take updates from queue
-
-
-# webhook = OrderedWebhook(bot, {'chat': on_chat_message})
+webhook = OrderedWebhook(bot, {'chat': on_chat_message})
+webhook.run_as_thread()
 logger = logging.getLogger('bot')
 
+import codecs
 
+
+
+import json
 def index(request):
-    logger.info(request.POST)
-    # logger.info(request.body)
-    # if request.POST:
-    #     logger.info(webhook)
-    logger.info(update_queue)
+
     try:
-        # logger.info(request.body)
-        # json_object = json.loads(request.body)
-        # logger.info('json')
-        update_queue.put(request.body)  # pass update to bot
-    except TypeError:
-        logger.info('update queue error')
+        bod = request.body
+        bod = bod.decode("utf-8")
+        logger.info(bod)
+        bod = json.loads(bod)
+
+        logger.info('req')
+        webhook.feed(bod)
+    except ValueError:
+        logger.info('GET requested')
 
     # webhook.feed(request.body)
     return HttpResponse("Hello, world. You're at the bot index.")
