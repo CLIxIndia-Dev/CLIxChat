3126ae3cc4eb191388ac31879c91ea12d5c4b736
Brandon <bhanks@mit.edu>
2017-06-19 22:49:15 -0400

    more debug msg

--- bot/views.py
+++ bot/views.py
@@ -52,6 +52,7 @@ def geturl(ext, x, buttons, chat_id):
 def on_chat_message(msg):
     logger.info('on_chat_message called')
     logger.info(msg)
+    logger.info(type(msg))
     content_type, chat_type, chat_id = telepot.glance(msg)
     logger.info('Chat Message: ', content_type, chat_type, chat_id)
 
